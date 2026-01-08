8e0eb3138d976efc249675c7c343003546d609f8
roi-sucgang <sucgangroi@gmail.com>
2026-01-08 22:29:23 +0800

    inital commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState } from "react"
 
-const Navbar = () => {
+const Navbar = ({ darkMode, toggleDarkMode}) => {
+  const [activeSection, setActiveSection] = useState('home');
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const navItems = [
+    {name: 'Home', link: '#home'},
+    {name: 'About', link: '#about'},
+    {name: 'Contact', link: '#contact'},
+  ]
   return (
     <div>
-      
+      Navbar
     </div> 
   )
 }
